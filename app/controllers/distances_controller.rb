2c95e4c5f73ffc430f4868ba3fae25f01316c62d
Rahul Patil <rahulpatil2387@gmail.com>
2017-07-10 14:53:04 +0530

    Add code to communicate to zipcodeapi and calculate distance

--- app/controllers/distances_controller.rb
+++ app/controllers/distances_controller.rb
@@ -1,4 +1,23 @@
 class DistancesController < ApplicationController
   def index
+    if params[:zip_code1] && params[:zip_code2]
+      url = "https://www.zipcodeapi.com/rest/#{ENV['API_KEY']}/distance.json/#{params[:zip_code1]}/#{params[:zip_code2]}/km"
+      response = get_distance_from_api(url)
+      begin
+        @calculated_distance = extract_distance(response)
+      rescue StandardError
+      end
+    end
+  end
+
+  private
+
+  def get_distance_from_api(url)
+    uri = URI(url)
+    Net::HTTP.get_response(uri)
+  end
+
+  def extract_distance(response)
+    JSON.parse(response.body)
   end
 end
