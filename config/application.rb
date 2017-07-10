5a4020d579e170e3e856927d64674ecc8dcf8315
Rahul Patil <rahulpatil2387@gmail.com>
2017-07-10 14:51:57 +0530

    Add api key related configurations

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module DistanceCalculator
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+
+    # preload tokens in application.yml to local ENV
+    config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+    config.merge! config.fetch(Rails.env, {})
+    config.each do |key, value|
+      ENV[key] = value.to_s unless value.kind_of? Hash
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
